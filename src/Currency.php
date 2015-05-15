1f95bd9a9ad288835bf11ba82a418cf907016dfa
MarvinKlemp <itklemp@googlemail.com>
2015-05-15 11:11:04 +0000

    Added phpdoc

--- src/Currency.php
+++ src/Currency.php
@@ -4,13 +4,23 @@ namespace MarvinKlemp\Money;
 
 class Currency
 {
+    /**
+     * @var string
+     */
     protected $name;
 
+    /**
+     * @param string $name
+     */
     public function __construct($name)
     {
         $this->name = $name;
     }
 
+    /**
+     * @param Currency $currency
+     * @return bool
+     */
     public function equals(Currency $currency)
     {
         return $this->name === $currency->name;
--- src/Money.php
+++ src/Money.php
@@ -4,10 +4,20 @@ namespace MarvinKlemp\Money;
 
 class Money
 {
+    /**
+     * @var float
+     */
     protected $amount;
 
+    /**
+     * @var Currency
+     */
     protected $currency;
 
+    /**
+     * @param float $amount
+     * @param Currency $currency
+     */
     private function __construct($amount, Currency $currency)
     {
         $this->amount = $amount;
@@ -23,6 +33,10 @@ class Money
         return new Money($arguments[0], new Currency($name));
     }
 
+    /**
+     * @param Money $money
+     * @return Money
+     */
     public function remove(Money $money)
     {
         if (!$this->currency->equals($money->currency)) {
@@ -32,6 +46,10 @@ class Money
         return new Money($this->amount - $money->amount(), $this->currency);
     }
 
+    /**
+     * @param Money $money
+     * @return Money
+     */
     public function add(Money $money)
     {
         if (!$this->currency->equals($money->currency)) {
@@ -41,11 +59,17 @@ class Money
         return new Money($this->amount + $money->amount(), $this->currency);
     }
 
+    /**
+     * @return float
+     */
     public function amount()
     {
         return $this->amount;
     }
 
+    /**
+     * @return Currency
+     */
     public function currency()
     {
         return $this->currency;
